3b4b6ea789c821b0bf0ba7c53a1d7f44bbd17bd0
Muhammad Nouraiz <muhammadnouraiz842@gmail.com>
2026-03-04 00:12:55 +0500

    configure root redux store

---
+++ client/src/store/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from './authSlice';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    // Future expansion: tools, rentals, chat
+  },
+});
+
+export default store;
